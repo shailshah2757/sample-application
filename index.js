83abd871181804d67356d87f6b60ccc43b90fcbe
20it135 <80442908+shailshah2757@users.noreply.github.com>
2023-02-21 14:05:20 +0530

    dockerfile added port updated

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-type':'text/html'});
     res.end("Hello world");
-}).listen(8080);
+}).listen(3000);
